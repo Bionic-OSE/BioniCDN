5c1da4903fb4a020643b2ade09a07bde2b6663f6
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2023-08-01 15:32:17 +0700

    20230801.001 - Upload #008

---
+++ Nekobox/Nekoinfo.ps1
@@ -0,0 +1,5 @@
+# Bionic's Nekobox package version information - (c) Bionic Butter
+
+$version = "1.0"
+$size = "166MB"
+$snum = "4"
--- YuumeiDKU/hikaru/Hikarinfo.ps1
+++
@@ -1,6 +0,0 @@
-# Hikaru-chan package version information - (c) Bionic Butter
-
-$version = "100_beta2"
-$minbase = "100_beta2"
-$size = "2.47MB"
-$descr = "Second beta of Hikaru for YuumeiDKU"
