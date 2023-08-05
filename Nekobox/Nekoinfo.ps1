73c663541bf6bce7f7aee19c7e6cc8fbdc935271
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2023-08-06 00:42:48 +0700

    20230806.001 - Upload #018

--- Nekobox/Nekoinfo.ps1
+++ Nekobox/Nekoinfo.ps1
@@ -1,5 +1,5 @@
 # Bionic's Nekobox package version information - (c) Bionic Butter
 
-$version = "1.1"
-$size = "288MB (Before updating, please open CMD and run this command: `"powershell Remove-Item $env:SYSTEMDRIVE\Bionic\Nekobox\Neko*.7z* -Force`""
-$snum = "6"
+$version = "1.2"
+$size = "312MB"
+$snum = "7"
