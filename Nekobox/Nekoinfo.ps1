3c59a7f09a9062eea08c9dfc5b06e39fcd76a236
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2024-03-06 02:21:18 +0700

    20240306.001 - Upload #030

--- Nekobox/Nekoinfo.ps1
+++ Nekobox/Nekoinfo.ps1
@@ -1,5 +1,5 @@
 # Bionic's Nekobox package version information - (c) Bionic Butter
 
-$version = "2.4"
-$size = "448MB"
-$snum = "9"
+$version = "2.5"
+$size = "472MB (please update OSXE before you update this!)"
+$snum = "10"
