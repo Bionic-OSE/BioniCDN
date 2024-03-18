78bbff033ba6d7480b8bd05991cb251e4ece1fb0
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2024-03-18 23:48:43 +0700

    20240318.004 - Upload #039

--- Kirisame/Nekobox3/Nekoinfo.ps1
+++ Kirisame/Nekobox3/Nekoinfo.ps1
@@ -1,6 +1,6 @@
 # Bionic's Nekobox package version information - (c) Bionic Butter
 
-$version = "3.1"
+$version = "3.2"
 $updatetype = 1
 $size = "523KB (Update) - 472MB (Full install)"
 $snum = "10"
