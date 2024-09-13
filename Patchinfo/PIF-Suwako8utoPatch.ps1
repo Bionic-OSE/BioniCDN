faebd80f9e7f2a13f8659168e789d4acc11d6400
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2024-09-13 08:21:53 +0700

    20240913.001 - Upload #054

--- Patchinfo/PIF-Suwako8utoPatch.ps1
+++ Patchinfo/PIF-Suwako8utoPatch.ps1
@@ -6,13 +6,13 @@ switch ($osbld) {
 		$ubr = 25073
 		$size = "465MB"
 		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/09/windows8-rt-kb5044412-x64_fc962d64ab7359364234d775faadd65645ea3c73.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/08/windows8-rt-kb5041851-x64_2bbdc923d8a43f3ae1cbcde76573760d9fa9137e.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/09/windows8-rt-kb5043125-x64_2cee240ee5074b2759e9baf0d186e644273c32f4.msu"
 	}
 	9600 {
 		$des = "2024-09 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5043138)"
 		$ubr = 22175
 		$size = "604MB"
 		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/09/windows8.1-kb5044410-x64_c5d94f684955082b510c07bfda6f8b9d7f31343d.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/08/windows8.1-kb5041828-x64_78a874c2f777d89bf6f3c8ea82ed00c1300bbf61.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/09/windows8.1-kb5043138-x64_eb332b003ced897d276223e2949a22bc9f6d5c00.msu"
 	}
 }
