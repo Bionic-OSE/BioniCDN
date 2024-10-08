daa7809b4d08808d728fe6be97caaeaf7aea14cb
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2024-10-09 00:24:42 +0700

    20241009.001 - Upload #061

--- Patchinfo/PIF-Suwako8utoPatch.ps1
+++ Patchinfo/PIF-Suwako8utoPatch.ps1
@@ -2,17 +2,17 @@
 
 switch ($osbld) {
 	9200 {
-		$des = "2024-09 Security Monthly Quality Rollup for x64 Windows 8 and Server 2012 (KB5043125)"
-		$ubr = 25073
-		$size = "465MB"
-		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/09/windows8-rt-kb5044412-x64_fc962d64ab7359364234d775faadd65645ea3c73.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/09/windows8-rt-kb5043125-x64_2cee240ee5074b2759e9baf0d186e644273c32f4.msu"
+		$des = "2024-10 Security Monthly Quality Rollup for x64 Windows 8 and Server 2012 (KB5044342)"
+		$ubr = 25118
+		$size = "468MB"
+		$ssu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/09/windows8-rt-kb5044413-x64_35aa1dea7b6776b82fcc4bb02765efb308ee984b.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/10/windows8-rt-kb5044342-x64_d7adb1575bf5b3306dbfb7dd8a31e40df694113a.msu"
 	}
 	9600 {
-		$des = "2024-09 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5043138)"
-		$ubr = 22175
-		$size = "604MB"
-		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/09/windows8.1-kb5044410-x64_c5d94f684955082b510c07bfda6f8b9d7f31343d.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/09/windows8.1-kb5043138-x64_eb332b003ced897d276223e2949a22bc9f6d5c00.msu"
+		$des = "2024-10 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5044343)"
+		$ubr = 22221
+		$size = "602MB"
+		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/09/windows8.1-kb5044411-x64_cc4a06a0f16ed5c09fe811ecdc5b19614418aaba.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/10/windows8.1-kb5044343-x64_1d0de69d775452a28de796eff0c0ad7f44365385.msu"
 	}
 }
