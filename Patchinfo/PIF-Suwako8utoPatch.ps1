78f4ad22095802255fed38d0552256535e3b7156
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2024-11-27 10:08:56 +0700

    20241127.001
    Repository reset

---
+++ Patchinfo/PIF-Suwako8utoPatch.ps1
@@ -0,0 +1,18 @@
+# Suwako8utoPatch - Patch information file - (c) Bionic Butter
+
+switch ($osbld) {
+	9200 {
+		$des = "2024-10 Security Monthly Quality Rollup for x64 Windows 8 and Server 2012 (KB5044342)"
+		$ubr = 25118
+		$size = "468MB"
+		$ssu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/09/windows8-rt-kb5044413-x64_35aa1dea7b6776b82fcc4bb02765efb308ee984b.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/10/windows8-rt-kb5044342-x64_d7adb1575bf5b3306dbfb7dd8a31e40df694113a.msu"
+	}
+	9600 {
+		$des = "2024-10 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5044343)"
+		$ubr = 22221
+		$size = "602MB"
+		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2024/09/windows8.1-kb5044411-x64_cc4a06a0f16ed5c09fe811ecdc5b19614418aaba.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2024/10/windows8.1-kb5044343-x64_1d0de69d775452a28de796eff0c0ad7f44365385.msu"
+	}
+}
