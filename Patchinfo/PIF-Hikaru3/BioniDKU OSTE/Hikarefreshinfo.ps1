68f211ddc88f348a31ba1c3ee317c29da826aedd
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2025-01-18 21:05:13 +0700

    20250118.002

--- Patchinfo/PIF-Hikaru3/BioniDKU OSTE/Hikarefreshinfo.ps1
+++ Patchinfo/PIF-Hikaru3/BioniDKU OSTE/Hikarefreshinfo.ps1
@@ -1,3 +1,4 @@
 # BioniDKU OSTE servicing components version information - (c) Bionic Butter
 
 $servicer = "2.3"
+$tag = "204_oste"
---
+++ Patchinfo/PIF-Hikaru3/BioniDKU OSTE/Hikarinfo.ps1
@@ -0,0 +1,25 @@
+# Hikaru-chan for OSTE package version information - (c) Bionic Butter
+
+$version = "204"
+$minbase = "204"
+$revision = "107"
+$size = "94.8MB"
+$descr = @"
+`r`n
+ This last update to your image brings it to revision (23020.)107, which includes apps updates and some other final
+ quality fixes. 
+ This update requires you to have the latest special standalone update package installed (don't worry, most of you 
+ did). This package is published in the announcement channel, so if you haven't, please do so before installing 
+ this update (otherwise the updater may screw itself up and BREAK your image).
+
+ These apps are getting their updates:
+ - Browser Tamer: 3.4.1 > 3.5.0
+ - K-Lite Codecs Pack (which MPC-HC is a part of): 17.6.8 > 17.7.5
+ 
+ In addition, these final changes are also worth mentioning:
+ - Adjusted the Explorer shell restarting mechanism once again, completely eliminating the chance of you seeing
+   a file window upon restarting on certain devices. The time taken to restart had to be slightly increased for
+   this however.
+ - Disabled the auto update check and hid the update checking entries in the menus.
+
+"@
