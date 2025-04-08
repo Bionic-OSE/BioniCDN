45e5b973c1399046d2b9f3ac35bbafbb7ef2e937
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2025-04-09 00:44:29 +0700

    20250409.001
    2024-05 Security Monthy Updates for Suwako8utoPatch clients

--- Patchinfo/PIF-Suwako8utoPatch.ps1
+++ Patchinfo/PIF-Suwako8utoPatch.ps1
@@ -2,17 +2,17 @@
 
 switch ($osbld) {
 	9200 {
-		$des = "2025-03 Security Monthly Quality Rollup for x64 Windows 8 and Server 2012 (KB5053886)"
-		$ubr = 25368
+		$des = "2025-04 Security Monthly Quality Rollup for x64 Windows 8 and Server 2012 (KB5055581)"
+		$ubr = 25423
 		$size = "466MB"
-		$ssu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/02/windows8-rt-kb5052109-x64_4dae2c2412dab65170016f04dc48795426e7b4f9.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/03/windows8-rt-kb5053886-x64_59f96e01b530abd39dab5d5d5801abcb8293d70d.msu"
+		$ssu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/03/windows8-rt-kb5055667-x64_1290c8eaae9083f3c19386b968ca01eda5115d8e.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/04/windows8-rt-kb5055581-x64_a0d7b803431ba132e201b1a9f43c1df17a14453d.msu"
 	}
 	9600 {
-		$des = "2025-03 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5053887)"
-		$ubr = 22470
+		$des = "2025-04 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5055557)"
+		$ubr = 22523
 		$size = "605MB"
-		$ssu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/02/windows8.1-kb5052108-x64_e50166a04f379fe064613833415862bac9be21c9.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/03/windows8.1-kb5053887-x64_bfcd076349572304fbb0877418db91915a574855.msu"
+		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/03/windows8.1-kb5055665-x64_dfa11621af869c05f448fe7b6d6c3cd02c30e0b9.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/04/windows8.1-kb5055557-x64_dcf073817ff8adc794e57ae190d4992f04d4a1d3.msu"
 	}
 }
