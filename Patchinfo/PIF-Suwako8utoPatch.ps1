cebd8162550f9d7c2df6a2ce000960249ef7c12e
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2025-08-13 00:20:30 +0700

    20250813.001

--- Patchinfo/PIF-Suwako8utoPatch.ps1
+++ Patchinfo/PIF-Suwako8utoPatch.ps1
@@ -3,16 +3,17 @@
 switch ($osbld) {
 	9200 {
 		$des = "2025-07 Security Monthly Quality Rollup for x64 Windows 8 and Server 2012 (KB5062592)"
-		$ubr = 25573
+		$ubr = 25622
 		$size = "~466MB"
 		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/05/windows8-rt-kb5058530-x64_d4038f22fef499833514ded6e1a0328ac68f481d.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/07/windows8-rt-kb5062592-x64_4dfac7d065497358b3315cf3ccb10af2e8cb0eaa.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/08/windows8-rt-kb5063906-x64_947c614d541b6fe2016dc6559affb8de0f136cb5.msu"
 	}
 	9600 {
 		$des = "2025-07 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5062597)"
-		$ubr = 22676
+		$ubr = 22725
 		$size = "~610MB"
 		$ssu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/05/windows8.1-kb5058529-x64_1d137b8edef44b970adcd971bf09483e7e16d767.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/07/windows8.1-kb5062597-x64_0589433d1eee127428cb4979264528297a5df4d2.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/08/windows8.1-kb5063950-x64_d743be306a7c9840e7c41cf2355564e3fea6d7e7.msu"
 	}
 }
+
