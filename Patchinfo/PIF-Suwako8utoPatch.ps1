bb1830a743d85463b5672e778ce60e78c99e5200
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2025-09-08 12:06:16 +0700

    20250908.001
    Reverting 8.x to 2025-06 CU due to 2025-07 and later breaking the OS.

--- Patchinfo/PIF-Suwako8utoPatch.ps1
+++ Patchinfo/PIF-Suwako8utoPatch.ps1
@@ -2,18 +2,17 @@
 
 switch ($osbld) {
 	9200 {
-		$des = "2025-07 Security Monthly Quality Rollup for x64 Windows 8 and Server 2012 (KB5062592)"
-		$ubr = 25622
+		$des = "2025-06 Security Monthly Quality Rollup for x64 Windows 8 and Server 2012 (KB5061059)"
+		$ubr = 25522
 		$size = "~466MB"
 		$ssu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/05/windows8-rt-kb5058530-x64_d4038f22fef499833514ded6e1a0328ac68f481d.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/08/windows8-rt-kb5063906-x64_947c614d541b6fe2016dc6559affb8de0f136cb5.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/06/windows8-rt-kb5061059-x64_0ac6afd2ba8f205fca5f53a831a3bf5818512be6.msu"
 	}
 	9600 {
-		$des = "2025-07 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5062597)"
-		$ubr = 22725
+		$des = "2025-06 Security Monthly Quality Rollup for x64 Windows 8.1 and Server 2012 R2 (KB5061018)"
+		$ubr = 22620
 		$size = "~610MB"
 		$ssu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/05/windows8.1-kb5058529-x64_1d137b8edef44b970adcd971bf09483e7e16d767.msu"
-		$lcu = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2025/08/windows8.1-kb5063950-x64_d743be306a7c9840e7c41cf2355564e3fea6d7e7.msu"
+		$lcu = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2025/06/windows8.1-kb5061018-x64_8a4c8110514a4526e29055fc963de7807cb6b906.msu"
 	}
 }
-
