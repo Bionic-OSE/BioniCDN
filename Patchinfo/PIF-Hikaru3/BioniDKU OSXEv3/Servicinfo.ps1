6db4d174c08e15607b33d7ce2ae285d0591758b8
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2026-01-18 13:12:50 +0700

    20260118.001

--- Patchinfo/PIF-Hikaru3/BioniDKU OSXEv3/Servicinfo.ps1
+++ Patchinfo/PIF-Hikaru3/BioniDKU OSXEv3/Servicinfo.ps1
@@ -1,5 +1,5 @@
 # BioniDKU OSXEv3 System Updater version information and update delivery control file - (c) Bionic Butter
 # (https://github.com/Bionic-OSE/BioniCDN/blob/main/Patchinfo/PIF-Hikaru3/BioniDKU%20OSXEv3/Servicinfo.ps1)
 
-$global:servicer = "6.2.3 (Luna III)"
-$global:tag = "303_osxev3"
+$global:servicer = "6.3 (Luna IV)"
+$global:tag = "304_osxev3"
